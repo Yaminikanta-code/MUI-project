4e48000b22126bd4c2d89713592717ab983691da
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-11 00:00:10 +0530

    add code

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -15,7 +15,7 @@ import React from "react";
 
 const Rightbar = () => {
   return (
-    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+    <Box flex={2} p={2} sx={{ display: { xs: "none", md: "block" } }}>
       <Box position="fixed" width={300}>
         <Typography variant="h6" fontWeight={100}>
           Online Friends
