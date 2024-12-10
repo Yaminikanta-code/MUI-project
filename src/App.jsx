d5b71a811a55c13bc1209438dd42d05fa0748d67
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-12-11 00:12:57 +0530

    add code

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import Sidebar from "./components/Sidebar.jsx";
 import Feed from "./components/Feed.jsx";
 import Rightbar from "./components/Rightbar.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Add from "./components/Add.jsx";
 import { Stack } from "@mui/material";
 function App() {
   return (
     <>
       <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Stack direction="row" spacing={0} justifyContent="space-between">
         <Sidebar />
         <Feed />
         <Rightbar />
       </Stack>
+      <Add />
     </>
   );
 }
